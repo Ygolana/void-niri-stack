cb54c8d975179298ffebdc44558032419a41d19e
Ygolana <villdreg@gmail.com>
2026-04-18 16:56:51 +0800

    Update base.sh

--- modules/base.sh
+++ modules/base.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 source lib/core.sh
 source lib/packages.sh
@@ -7,14 +8,28 @@ LOG "Updating system..."
 sudo xbps-install -Su
 
 LOG "Installing base system..."
+
 install_packages \
   git curl base-devel pkg-config \
   dbus elogind \
   wayland wayland-protocols \
-  mesa
+  mesa mesa-dri vulkan-loader \
+  glib-devel \
+  libseat-devel \
+  pipewire-devel \
+  cairo-devel \
+  pango-devel \
+  libdisplay-info-devel \
+  libxkbcommon-devel \
+  libinput-devel \
+  pixman-devel \
+  libdrm-devel \
+  eudev-libudev-devel \
+  clang llvm \
+  rust cargo
 
 LOG "Enabling services..."
-sudo ln -sf /etc/sv/dbus /var/service
-sudo ln -sf /etc/sv/elogind /var/service
+sudo ln -sf /etc/sv/dbus /var/service || true
+sudo ln -sf /etc/sv/elogind /var/service || true
 
 LOG "Base setup complete"
